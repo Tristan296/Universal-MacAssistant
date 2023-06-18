3f78c07b698f97b2bb099c4b23644b3160501ca6
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-06-18 18:41:51 +1000

    fixed note taker

--- src/core/voice_assistant.py
+++ src/core/voice_assistant.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 class VoiceAssistant:
     def __init__(self):
         # Set your OpenAI API key
-        openai.api_key = "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"
+        openai.api_key = "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"
         # Initialize the assistant's history
         self.history = [
             {
@@ -81,6 +81,11 @@ class VoiceAssistant:
                 self.speak("Goodbye! Have a great day!")
                 break
 
+            if "list" in formattedText or "note" in formattedText: 
+                from src.skills.todo_list import todoList
+                todolist = todoList(self)
+                todolist.create_todo_list()
+                
             if "reminder" in formattedText:
                 from src.skills.reminder import Reminder
                 reminder = Reminder(self)
--- src/skills/todo_list.py
+++ src/skills/todo_list.py
@@ -3,21 +3,21 @@ import time
 import pyautogui
 import platform
 import os, sys
-from core.voice_assistant import assistant
+from core.voice_assistant import VoiceAssistant 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 class todoList:
-    def __init__(self):
-        pass
+    def __init__(self, voice_assistant):
+        self.voice_assistant = voice_assistant
     
-    def create_todo_list():
+    def create_todo_list(self):
         todo_list = []
-        assistant.speak("Let's create a to-do list. Please say the tasks one by one. Say 'done' when you're finished.")
+        self.voice_assistant.speak("Let's create a to-do list. Please say the tasks one by one. Say 'done' when you're finished.")
 
         while True:
-            task = assistant.listen()
+            task = self.voice_assistant.listen()
             
             if "done" in task.strip().lower():
                 break
@@ -36,3 +36,8 @@ class todoList:
                 pyautogui.hotkey("return")
             elif platform.system() == "Windows":
                 os.system("notepad")
+                
+if __name__ == "__main__": 
+    voice_assistant = VoiceAssistant()
+    reminder = todoList(voice_assistant)
+    reminder.set_reminder()
