0b1488e74e2b60457fecea7559257a6d47370971
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-06-24 12:31:46 +1000

    Major improvements: LangChain engine and command Implemented

--- src/skills/weather.py
+++ src/skills/weather.py
@@ -77,7 +77,7 @@ class Weather:
         return chosen_city
 
     def find_weather(self, city_name):
-        openweather_api_key = "API_key_here"
+        openweather_api_key = "73e9748c0e68dc34e3ae90236f82d642"
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={openweather_api_key}"
 
         try:
