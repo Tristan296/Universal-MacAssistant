6771d0621fdcc04933264594a68575e86468b90f
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-07-10 22:24:38 +1000

    added the ability to open files

--- src/core/voice_assistant.py
+++ src/core/voice_assistant.py
@@ -1,5 +1,6 @@
 import re
 import time
+from dotenv import load_dotenv
 import numpy as np
 import openai
 import sounddevice as sd
@@ -7,26 +8,21 @@ from scipy.io import wavfile
 import tempfile
 import subprocess
 from gtts import gTTS
-import os, sys
-from langchain.agents import initialize_agent
-from langchain.llms import OpenAI
-from dotenv import load_dotenv
-from langchain.agents import load_tools
+import os
+import sys
 import speech_recognition as sr
-
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from skills.apple_scripts import ask_siri
+from skills.apple_scripts import ask_siri, open_file
 from core.commands import (computer_applescript_action,
-            chrome_open_url,
-            chrome_get_the_links_on_the_page,
-            chrome_read_the_page,
-            chrome_click_on_link)
-
-# load environment variables
+                           chrome_open_url,
+                           chrome_get_the_links_on_the_page,
+                           chrome_read_the_page,
+                           chrome_click_on_link)
 load_dotenv()
+
 class VoiceAssistant:
     def __init__(self):
         # Set your OpenAI API key
@@ -39,15 +35,6 @@ class VoiceAssistant:
                 "content": "You are a helpful assistant. The user is English. Only speak English.",
             }
         ]
-        llm = OpenAI(temperature=0, openai_api_key=api_key) # type: ignore 
-        tools = [
-            computer_applescript_action,
-            chrome_open_url,
-            chrome_get_the_links_on_the_page,
-            chrome_read_the_page,
-            chrome_click_on_link
-        ]
-        self.agent = initialize_agent(tools, llm, initialize_agent="zero-shot-react-description", verbose=True)
 
     def listen(self):
         """
@@ -72,28 +59,6 @@ class VoiceAssistant:
             print("Sorry, I couldn't understand what you said.")
             return ""
 
-    def think(self, text):
-        """
-        Generates a response to the user's input.
-        """
-        # Add the user's input to the assistant's history
-        self.history.append({"role": "user", "content": text})
-
-        # Use the agent to generate a response
-        response = self.agent.run(text)
-
-        if isinstance(response, str):
-            # Handle the case when the response is a string
-            message = response
-        else:
-            # Extract the assistant's response from the agent output
-            message = response["content"]
-
-        self.history.append({"role": "system", "content": message})
-        print("Assistant:", message)
-        return message
-
-
     def speak(self, text):
         # Convert text to speech using gTTS
         tts = gTTS(text=text, lang='en', slow=False)
@@ -105,7 +70,6 @@ class VoiceAssistant:
         # Play the audio file through the speakers
         subprocess.run(["afplay", temp_audio.name])
 
-    
     def print_typing(self, text):
         for char in text:
             print(char, end='', flush=True)
@@ -114,8 +78,8 @@ class VoiceAssistant:
 
     def run(self):
         while True:
-            print("""Initialising ChatGPT, Text-To-Speech and LangChain...\n""")
-            print("How can I assist you? If unsure say help")
+            print("""Initialising ChatGPT, Text-To-Speech...\n""")
+            print("How can I assist you? If unsure, say help")
 
             text = self.listen()
             formattedText = text.strip().lower() if isinstance(text, str) else ""
@@ -125,37 +89,40 @@ class VoiceAssistant:
                 self.speak("Goodbye! Have a great day!")
                 break
 
-            elif "list" in formattedText or "note" in formattedText: 
+            elif "list" in formattedText or "note" in formattedText:
                 from src.skills.todo_list import todoList
+
                 todolist = todoList(self)
                 todolist.create_todo_list()
                 break
 
-            elif "speed" in formattedText or "internet speed" in formattedText: 
+            elif "speed" in formattedText or "internet speed" in formattedText:
                 from src.skills.internet_test import InternetSpeed, SpeedHistory
+
                 history_file_path = "speed_history.json"
                 speed_history = SpeedHistory(history_file_path)
                 speed = InternetSpeed(self, speed_history)
                 speed.run()
-                            
+
             elif "weather" in formattedText:
                 from src.skills.weather import Weather
+
                 weather = Weather(self, None)
                 weather.run()
-            
-            elif ("open" in formattedText or "reminder" in formattedText):
+
+            elif (("open" in formattedText and "app" in formattedText) or "reminder" in formattedText):
                 ask_siri(formattedText)
-            
-            elif re.search(r"(increase|decrease|reduce) brightness", formattedText) or re.search(r"(increase|decrease|reduce) volume", formattedText):
+
+            elif re.search(r"(increase|decrease|reduce) brightness", formattedText) or re.search(
+                    r"(increase|decrease|reduce) volume", formattedText):
                 ask_siri(formattedText)
-            
+
             elif re.search(r"(wifi|bluetooth|settings)", formattedText):
                 ask_siri(formattedText)
-                
+
+            elif "open" in formattedText and "file" in formattedText:
+                open_file(formattedText)
+
             elif "exit" in formattedText or "quit" in formattedText:
                 print("Goodbye")
                 break
-
-            response = self.think(text)
-            self.speak(response)
-
--- src/skills/apple_scripts.py
+++ src/skills/apple_scripts.py
@@ -1,4 +1,7 @@
 import subprocess
+import re 
+import os
+
 
 def spotlight_search(query):
     script = f'''
@@ -14,7 +17,7 @@ def spotlight_search(query):
         print(f"Error: {error.strip()}")
         return []
 
-    search_results = output.strip().split('\n')
+    search_results = output.strip().split(',')
     return search_results
 
 def ask_siri(prompt):
@@ -32,3 +35,35 @@ def ask_siri(prompt):
     process = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     process.communicate(script)
 
+
+def filter_search(query, results):
+    extensions = {}
+
+    # Iterate over each path and extract the file name and extension
+    for path in results:
+        basename = os.path.basename(path)
+        result = re.sub(r'\.\w+$', '', basename)
+        extension = os.path.splitext(basename)[1]
+        if extension == "":
+            break
+        extensions[path] = {'file_name': result, 'file_extension': extension}
+    
+    return extensions
+
+def open_file(query):
+    # Extract the file name from the query
+    match = re.search(r'open\s+(\S+)', query)
+    if match:
+        file = match.group(1)
+        results = spotlight_search(file)
+        filtered_results = filter_search(query, results)
+
+        # Check if any matching files are found
+        if filtered_results:
+            file_path = next(iter(filtered_results))  # Get the first matching file path
+            subprocess.call(['open', file_path])
+            quit()
+        else:
+            print("No matching files found.")
+    else:
+        print("Invalid command format. Please provide the file name.")
